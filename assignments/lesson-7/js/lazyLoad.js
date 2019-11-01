7b10d7057271c6f6095171c7f47ac069a9c612c5
jillraz99.github.io <jillraz99@gmail.com>
2019-10-31 18:18:37 -0700

    updates

--- assignments/lesson-7/js/lazyLoad.js
+++ assignments/lesson-7/js/lazyLoad.js
@@ -7,33 +7,30 @@ const imgOptions = {
     rootMargin: "0px 0px 50px 0px"
 };
 
+//set grounds for Intersection Observer
 const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
     image.onload = () => {
         image.removeAttribute('data-src');
-    };
-};
-
-//first check to see if Intersection Observer is supported
-if ('IntersectionObserver' in window) {
-    const imgObserver = new IntersectionObserver((items, observer) => {
-        if (item.isIntesecting) {
-            loadedImages(item.target);
-            imgObserver.unobserve(item.target);
-        }
-    });
-}else {
-        imagesToLoad.forEach((img) => {
-            loadImages(img);
-        });
     }
+}
 
+//first check to see if Intersection Observer is supported and display when in window
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, imgObserver) => {
+        items.forEach((item) => {
 
-
-    //loop through each img and check status and load if necessary
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-});
-
-
-
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    });
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    })
+};
