88b9762c43723070880421d89dba03b505dd5ed0
jillraz99.github.io <jillraz99@gmail.com>
2019-12-06 12:58:54 -0800

    updates

--- assignments/temple-project/js/temples.js
+++ assignments/temple-project/js/temples.js
@@ -1,21 +1,31 @@
-const requestJson = 'temples.json';
+const requestJson = 'https://jillraz99.github.io/assignments/temple-project/temples.json';
 
 fetch(requestJson)
     .then(function (response) {
         return response.json();
     })
     .then(function (jsonObject) {
+        console.log(jsonObject);
+
         const temples = jsonObject['temples'];
         for (let i = 0; i < temples.length; i++) {
-            if (temple[i].name == 'Las Vegas') {
+            if (temples[i].name == 'Las Vegas') {
+
                 let card = document.createElement('section');
-                let h3 = document.createElement('h3');
-                let line = document.createElement('hr');
+                let h2 = document.createElement('h2');
                 let p1 = document.createElement('p');
                 let p2 = document.createElement('p');
                 let p3 = document.createElement('p');
+                let p4 = document.createElement('p');
+                let p5 = document.createElement('p');
+                let p6 = document.createElement('p');
+                let p7 = document.createElement('p');
+                let p8 = document.createElement('p');
+                let p9 = document.createElement('p');
+                let p10 = document.createElement('p');
+                let p11 = document.createElement('p');
 
-                h3.textContent = "Temple Closures";
+                h2.textContent = "Temple Closures";
                 p1.textContent = temples[i].closures[0];
                 p2.textContent = temples[i].closures[1];
                 p3.textContent = temples[i].closures[2];
@@ -28,7 +38,7 @@ fetch(requestJson)
                 p10.textContent = temples[i].closures[9];
                 p11.textContent = temples[i].closures[10];
 
-                card.appendChild(h3);
+                card.appendChild(h2);
                 card.appendChild(line);
                 card.appendChild(p1);
                 card.appendChild(p2);
@@ -42,7 +52,7 @@ fetch(requestJson)
                 card.appendChild(p10);
                 card.appendChild(p11);
 
-                document.querySelector('#vegas-closures').appendChild(card);
+                document.querySelector('div.closures').appendChild(card);
             }
         }
     });
